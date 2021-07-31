60696de500dc13f8832c7491b3c5a23a43254c0f
DESKTOP-R375PKT\sanghun <bigyou00@gmail.com>
2021-07-31 21:15:23 +0900

    헤더 문제 해결

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,3 +1,61 @@
+// import React from "react";
+// import styled from "styled-components";
+// import MuscleLink from "./components/MusclesLink";
+
+// const Main = () => {
+//   return (
+//     <Wrapper>
+//       <h1>부위별 운동 방법</h1>
+//       <ButtonBox>
+//         <MuscleButton>
+//           <MuscleLink address="/muscles/chest" muscleKoreaName="가슴" />{" "}
+//         </MuscleButton>
+//         <MuscleButton>
+//           <MuscleLink address="/muscles/back" muscleKoreaName="등" />
+//         </MuscleButton>
+//         <MuscleButton>
+//           <MuscleLink address="//muscles/shoulder" muscleKoreaName="어깨" />
+//         </MuscleButton>
+//         <MuscleButton>
+//           <MuscleLink address="//muscles/lowerBody" muscleKoreaName="하체" />
+//         </MuscleButton>
+//       </ButtonBox>
+//       <ButtonBox>
+//         <MuscleButton>
+//           <MuscleLink address="/muscles/triceps" muscleKoreaName="삼두" />
+//         </MuscleButton>
+//         <MuscleButton>
+//           <MuscleLink address="//muscles/biceps" muscleKoreaName="이두" />
+//         </MuscleButton>
+//         <MuscleButton>
+//           <MuscleLink address="/muscles/abs" muscleKoreaName="복근" />
+//         </MuscleButton>
+//       </ButtonBox>
+//     </Wrapper>
+//   );
+// };
+// const Wrapper = styled.div`
+//   display: flex;
+//   flex-flow: column;
+//   justify-content: center;
+//   align-items: center;
+//   width: 100vw;
+//   height: 100vh;
+// `;
+// const ButtonBox = styled.div`
+//   display: block;
+// `;
+// const MuscleButton = styled.button`
+//   width: 250px;
+//   height: 250px;
+//   margin: 50px;
+//   border-radius: 50%;
+//   transition: transform, 500ms ease-in-out;
+//   &:hover {
+//     transform: scale(1.2);
+//   }
+// `;
+// export default Main;
 import React from "react";
 import styled from "styled-components";
 import MuscleLink from "./components/MusclesLink";
@@ -7,29 +65,43 @@ const Main = () => {
     <Wrapper>
       <h1>부위별 운동 방법</h1>
       <ButtonBox>
-        <MuscleButton>
-          <MuscleLink address="/muscles/chest" muscleKoreaName="가슴" />{" "}
-        </MuscleButton>
-        <MuscleButton>
-          <MuscleLink address="/muscles/back" muscleKoreaName="등" />
-        </MuscleButton>
-        <MuscleButton>
-          <MuscleLink address="//muscles/shoulder" muscleKoreaName="어깨" />
-        </MuscleButton>
-        <MuscleButton>
-          <MuscleLink address="//muscles/lowerBody" muscleKoreaName="하체" />
-        </MuscleButton>
+        <MuscleLink
+          address="/muscles/chest"
+          muscleKoreaName="가슴"
+          circle={true}
+        />{" "}
+        <MuscleLink
+          address="/muscles/back"
+          muscleKoreaName="등"
+          circle={true}
+        />
+        <MuscleLink
+          address="/muscles/shoulder"
+          muscleKoreaName="어깨"
+          circle={true}
+        />
+        <MuscleLink
+          address="/muscles/lowerBody"
+          muscleKoreaName="하체"
+          circle={true}
+        />
       </ButtonBox>
       <ButtonBox>
-        <MuscleButton>
-          <MuscleLink address="/muscles/triceps" muscleKoreaName="삼두" />
-        </MuscleButton>
-        <MuscleButton>
-          <MuscleLink address="//muscles/biceps" muscleKoreaName="이두" />
-        </MuscleButton>
-        <MuscleButton>
-          <MuscleLink address="/muscles/abs" muscleKoreaName="복근" />
-        </MuscleButton>
+        <MuscleLink
+          address="/muscles/triceps"
+          muscleKoreaName="삼두"
+          circle={true}
+        />
+        <MuscleLink
+          address="/muscles/biceps"
+          muscleKoreaName="이두"
+          circle={true}
+        />
+        <MuscleLink
+          address="/muscles/abs"
+          muscleKoreaName="복근"
+          circle={true}
+        />
       </ButtonBox>
     </Wrapper>
   );
@@ -45,14 +117,5 @@ const Wrapper = styled.div`
 const ButtonBox = styled.div`
   display: block;
 `;
-const MuscleButton = styled.button`
-  width: 250px;
-  height: 250px;
-  margin: 50px;
-  border-radius: 50%;
-  transition: transform, 500ms ease-in-out;
-  &:hover {
-    transform: scale(1.2);
-  }
-`;
+
 export default Main;
--- src/pages/Main/components/MusclesLink.jsx
+++ src/pages/Main/components/MusclesLink.jsx
@@ -1,8 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
-const MusclesLink = ({ address, muscleKoreaName }) => {
-  return <Link to={address}>{muscleKoreaName}</Link>;
+const MusclesLink = ({ address, muscleKoreaName, circle }) => {
+  return (
+    <Link to={address}>
+      {circle ? (
+        <MuscleButton>{muscleKoreaName}</MuscleButton>
+      ) : (
+        <div>{muscleKoreaName}</div>
+      )}
+    </Link>
+  );
+};
+MusclesLink.defaultProps = {
+  circle: false,
 };
 
+const MuscleButton = styled.button`
+  width: 250px;
+  height: 250px;
+  margin: 50px;
+  border-radius: 50%;
+  transition: transform, 500ms ease-in-out;
+  &:hover {
+    transform: scale(1.2);
+  }
+`;
 export default MusclesLink;
